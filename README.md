afaf5629b2982cc38d11f5a0ea92248c364427af
Srinath10X <srinathcreates@gmail.com>
2023-12-30 18:08:43 +0530

    Updated the script and readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # termux-setup
+
 🚀 Welcome to Termux-Setup, your swift path to a hassle-free Termux configuration! This tool automates the setup process, ensuring a quick and efficient installation of essential packages. 📦 Say goodbye to manual setups and embrace a seamless start to your Termux journey. Get your environment up and running in no time with Termux-Setup! 🔧✨
+
+# Installation
+
+```bash
+wget -O - https://raw.githubusercontent.com/Srinath10X/termux-setup/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
+termux-setup-storage
 
-# Specify the backup directory
-backup_dir="$HOME/.termux-backup"
+apt update
+apt install -y git zsh
+git clone https://github.com/Srinath10X/termux-setup.git "$HOME/termux-setup" --depth 1
 
-# Check if .termux directory exists
-if [ -d "$HOME/.termux" ]; then
-    echo "Creating backup directory..."
-    mkdir -p "$backup_dir"
+mv "$HOME/.termux" "$HOME/.termux.backup"
+cp -R "$HOME/termux-setup/.termux" "$HOME/.termux"
 
-    echo "Taking backup of .termux folder..."
-    cp -r "$HOME/.termux" "$backup_dir"
-else
-    echo "The .termux directory does not exist. No backup was created."
-fi
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh" --depth 1
+mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
+cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
+sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
+sed -i '1iZSH_THEME="robbyrussell"' "$HOME/.zshrc"
 
-echo "Installing dependencies..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
 
-echo "Copying files..."
+chsh -s zsh
 
-echo "Configuring the application..."
+echo "Oh-my-zsh and some additional colorschemes are installed. Please restart Termux app..."
 
-echo "Installation complete!"
+termux-reload-settings
+
+echo "Restart Termux app..."
+
+exit
